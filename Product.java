05c3185339820f2818fe950027409f9442f71a70
likhovidIlya <62901662+likhovidIlya@users.noreply.github.com>
2023-05-01 19:20:39 +0300

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,19 @@
+class Product {
+    int price;
+    String name;
+
+
+
+    Product(int price, String name) {
+        this.price = price;
+        this.name = name;
+
+}
+
+
+    public void initProduct(int price, String name) {
+        this.price = price;
+        this.name = name;
+
+    }
+}
